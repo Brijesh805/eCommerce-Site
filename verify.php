c00007b0161001c01d112d63e97a5b935cb884f0
Brijesh805 <brijesh805402@gmail.com>
2025-06-28 14:47:44 +0530

    version2

--- verify.php
+++ verify.php
@@ -29,7 +29,7 @@ if ( (!isset($_REQUEST['email'])) || (isset($_REQUEST['token'])) )
 
 <div class="page-banner" style="background-color:#444;">
     <div class="inner">
-        <h1>Registration Successful</h1>
+        <h1>Registration Successfuls</h1>
     </div>
 </div>
 
